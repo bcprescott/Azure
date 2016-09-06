69bb7721e8b3e868ab1778c1fd18d73de12fc3b0
Ben Prescott <bcprescott@outlook.com>
2016-09-06 16:16:23 -0500

    Delete ARM_Deploy.ps1

--- ARM_Deploy.ps1
+++
@@ -1,6 +0,0 @@
-﻿Login-AzureRmAccount
-
-$deployName = "ARM Template Test"
-$templatepath = ".\LoadBalancedVirtualMachine.json"
-$parametersfile = ".\LoadBalancedVirtualMachine.parameters.json"
-New-AzureRmResourceGroupDeployment -ResourceGroupName "BP-RG" -TemplateFile $templatePath -TemplateParameterFile $parametersFile
