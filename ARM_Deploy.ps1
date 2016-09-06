d63658fff24a795690cc5d33b9b3bc490cd7e54c
Ben Prescott <bcprescott@outlook.com>
2016-09-06 15:48:07 -0500

    Add files via upload

---
+++ ARM_Deploy.ps1
@@ -0,0 +1,6 @@
+﻿Login-AzureRmAccount
+
+$deployName = "ARM Template Test"
+$templatepath = ".\LoadBalancedVirtualMachine.json"
+$parametersfile = ".\LoadBalancedVirtualMachine.parameters.json"
+New-AzureRmResourceGroupDeployment -ResourceGroupName "BP-RG" -TemplateFile $templatePath -TemplateParameterFile $parametersFile
